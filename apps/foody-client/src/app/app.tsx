d95040c44df599ebc635e20e3fd0ff5643adc7a6
Ali <ali.gusejnzade.01@mail.ru>
2024-07-17 10:52:13 +0400

    style: add global styles

--- apps/foody-client/src/app/app.tsx
+++ apps/foody-client/src/app/app.tsx
@@ -1,14 +1,11 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import styles from './app.module.scss';
+import  './app.module.scss';
 
-import NxWelcome from './nx-welcome';
 
 export function App() {
-  return (
-    <div>
-      <NxWelcome title="foody-client" />
-    </div>
-  );
+  return <p>
+    Ali
+  </p>;
 }
 
 export default App;
