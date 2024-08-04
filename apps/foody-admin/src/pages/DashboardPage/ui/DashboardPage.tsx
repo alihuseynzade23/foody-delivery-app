c00011c1ef278595de7bc9ba90ce5ec4186aa083
Ali Huseynzade <ali.huseynzade@azal.az>
2024-08-04 16:44:53 +0400

    fix: small fixes

--- apps/foody-admin/src/pages/DashboardPage/ui/DashboardPage.tsx
+++ apps/foody-admin/src/pages/DashboardPage/ui/DashboardPage.tsx
@@ -1,16 +1,8 @@
-import { useAuth } from '@org/foody-shared-components';
 import { OrdersChart } from '../../../../../foody-admin/src/shared/OrdersChart';
 import { TotalSalaryChart } from '../../../shared/TotalSalaryChart';
 import styles from './DashboardPage.module.scss';
-import React, { useEffect } from 'react';
 
 export const DashboardPage = () => {
-  const { user } = useAuth();
-
-  useEffect(() => {
-    console.log(user);
-  }, [user]);
-
   return (
     <div>
       <div className={styles.dashboardContainer}>
